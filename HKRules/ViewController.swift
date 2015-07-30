61c12d1fa6519b9f5f25c73025f3e84303959571
Tyler Freckmann <tylerfreckmann@SparkPR-Image-3.local>
2015-07-29 18:20:49 -0700

    Initial Commit

---
+++ HKRules/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HKRules
+//
+//  Created by Tyler Freckmann on 7/29/15.
+//  Copyright (c) 2015 Tyler Freckmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
