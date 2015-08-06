9456b1f17a6a44d31c439728f4e5aadda267b059
Eric Tan <Eric@SparkPR-Image.local>
2015-08-06 11:11:34 -0700

    finished and working showerStarted Cloud Code

--- CloudCode/cloud/main.js
+++ CloudCode/cloud/main.js
@@ -1,10 +1,9 @@
-
 // Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
 Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+  response.success("Goodbye world!");
 });
 
+// Sets the alarm in the cloud, and notifies user of the result through push notifcation. 
 Parse.Cloud.define("setCloudAlarm", function(request, response) {
 	var alarmDate = new Date(request.params.alarmTime);
 	var userQuery = new Parse.Query(Parse.User);
@@ -36,4 +35,49 @@ Parse.Cloud.define("setCloudAlarm", function(request, response) {
 			response.error("user find query errored");
 		}
 	});
+});
+
+// Called to push notification to HKRules application after shower timer triggered.
+Parse.Cloud.define("showerStarted", function(request, response) {
+
+	var userQuery = new Parse.Query(Parse.User);        
+	userQuery.equalTo("username", request.params.username);
+
+	userQuery.find({
+        success: function(users) {
+                   
+        // Get the current time
+        var alertTime = new Date();
+        alertTime.getHours();
+        alertTime.getMinutes();
+        alertTime.setSeconds(alertTime.getSeconds());
+                            
+        // Init the push query
+        var pushQuery = new Parse.Query(Parse.Installation);
+        pushQuery.equalTo("user", users[0]);
+        pushQuery.equalTo("appName", "HKRules")
+                     
+        // Push to HKRules phone
+        Parse.Push.send({
+             where: pushQuery,
+             data: {
+                 "alert": "Shower ALERT",
+                 "content-available": 1,
+             },
+             push_time: alertTime
+        },  {
+            success: function() {
+                 response.success("push for " + request.params.username + " scheduled.");
+            },
+            error: function(error) {
+                 response.error("push errored");
+            }
+        }); //end push
+                   
+    },
+        error: function(error) {
+		    response.error("user find query errored");
+        }
+                   
+    });
 });
