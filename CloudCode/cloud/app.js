79d52f57cf00e2bfa5e0e7966337b6da167b249e
Tyler Freckmann <tylerfreckmann@SparkPR-Image-3.local>
2015-08-07 16:16:08 -0700

    lala

--- CloudCode/cloud/app.js
+++ CloudCode/cloud/app.js
@@ -12,7 +12,7 @@ var Buffer = require('buffer').Buffer;
 var smartThingsClientId = '04e5f073-c826-482f-a6b5-e22e7d2d61fe';
 var smartThingsClientSecret = '908091ac-6690-4349-8cf1-592b970a00ec';
 
-var oauthCallback = 'http://hkrules.parseapp.com/oauthCallback';
+var oauthCallback = 'http://hkrules.parseapp.com/oauthCallback?usr=';
 
 // Global app configuration section
 app.set('views', 'cloud/views');  // Specify the folder to find templates
@@ -22,7 +22,8 @@ app.use(express.bodyParser());    // Middleware for reading request body
 // Login with SmartThings.
 // When called, generate a request token and redirect the browser to GitHub.
 app.get('/authorize', function(req, res) {
-	res.redirect('https://graph.api.smartthings.com/oauth/authorize?response_type=code&client_id='+smartThingsClientId+'&scope=app&redirect_uri='+oauthCallback);
+	console.log("authorize: "+req.query.usr);
+	res.redirect('https://graph.api.smartthings.com/oauth/authorize?response_type=code&client_id='+smartThingsClientId+'&scope=app&redirect_uri='+oauthCallback+req.query.usr);
 });
 
 // OAuth Callback route.
@@ -30,14 +31,37 @@ app.get('/authorize', function(req, res) {
 app.get('/oauthCallback', function(req, res) {
 	var u = new Buffer(smartThingsClientId+':'+smartThingsClientSecret).toString('base64');
 	Parse.Cloud.httpRequest({
-		url: 'https://graph.api.smartthings.com/oauth/token?code='+req.query.code+'&grant_type=authorization_code&redirect_uri=https://graph.api.smartthings.com/oauth/callback&scope=app',
+		url: 'https://graph.api.smartthings.com/oauth/token?code='+req.query.code+'&grant_type=authorization_code&redirect_uri='+oauthCallback+req.query.usr+'&scope=app',
 		headers: {
 			'Authorization': 'Basic '+u
 		}
 	}).then(function(httpResponse) {
+		var userQuery = new Parse.Query(Parse.User);
+		console.log("this is the username: "+req.query.usr);
+		userQuery.equalTo("username", req.query.usr);
+		userQuery.find({
+			success: function(users) {
+				var user = users[0];
+				console.log(users);
+				user.save({
+					sttoken: httpResponse.data.access_token
+				}, {
+					success: function(user) {
+						res.render('res', { message: 'Success! Please click back in the navigation bar.'});
+					},
+					error: function(user, error) {
+						res.render('res', {message: 'user save error: '+error.message+error.code});
+					}
+				});
+			},
+			error: function(error) {
+				res.render('res', {message: 'user find error: '+error.message});
+			}
+		});
 		console.log(httpResponse.data);
 	},function(httpResponse) {
 		console.error(httpResponse.text);
+		res.render('res', {message: 'http error: '+error.message});
 	});
 });
 
