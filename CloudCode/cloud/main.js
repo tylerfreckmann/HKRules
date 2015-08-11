0e57c83bcf539e4055e4b2abebd6f1192e81e4b9
Eric Tan <Eric@SparkPR-Image.local>
2015-08-11 14:30:18 -0700

    cleaned up code more (showerStarted)

--- CloudCode/cloud/main.js
+++ CloudCode/cloud/main.js
@@ -10,7 +10,7 @@ Parse.Cloud.define("setCloudAlarm", function(request, response) {
     var greetingURL;
     wakeConfig.fetch().then(function(wakeConfig) {
         // Get greeting
-        var greeting = wakeConfig.get("greeting").replace(' ', '+');
+        var greeting = wakeConfig.get("greeting").split(" ").join("%20");
         var ttsURL = baseSpeechURL+greeting+"&return_url=1";
         return Parse.Cloud.httpRequest({ url: ttsURL});
     }).then(function(httpResponse) {
@@ -41,60 +41,47 @@ Parse.Cloud.define("setCloudAlarm", function(request, response) {
 // Called to push notification to HKRules application after shower timer triggered.
 Parse.Cloud.define("showerStarted", function(request, response) {
 
-	var userQuery = new Parse.Query(Parse.User);        
-	userQuery.equalTo("username", request.params.username);
-
-	userQuery.find({
-        success: function(users) {
-                   
-        // Get the current time
-        var alertTime = new Date();
-        alertTime.getHours();
-        alertTime.getMinutes();
-        alertTime.getSeconds();
-                            
-        // Init the push query
-        var pushQuery = new Parse.Query(Parse.Installation);
-        pushQuery.equalTo("user", users[0]);
-        pushQuery.equalTo("appName", "HKRules")
-                   
-        var ttsURL = baseSpeechURL 
-                    + "Alert Alert Alert Alert Alert You have showered for ".split(" ").join("%20") 
-                    + request.params.showerTime 
-                    + "&return_url=1";
-
-        Parse.Cloud.httpRequest({
-            url: ttsURL,
-            success: function(httpResponse) {
-
-                // Push to HKRules phone
-                Parse.Push.send({
-                    where: pushQuery,
-                    data: {
-                        "alert": "You showered for " + request.params.showerTime,
-                        "content-available": 1,
-                        "ttsURL":  httpResponse.text
-                    },
-                        push_time: alertTime
-                },{ success: function() {
-                    response.success("push for " + request.params.username + " scheduled.");
+	var user = Parse.User.current();
+               
+    // Get the current time
+    var alertTime = new Date();
+    alertTime.getHours();
+    alertTime.getMinutes();
+    alertTime.getSeconds();
+                        
+    var pushQuery = new Parse.Query(Parse.Installation);
+    pushQuery.equalTo("user", user);
+    pushQuery.equalTo("appName", "HKRules")                   
+    
+    var ttsURL = baseSpeechURL 
+                + "Alert Alert Alert Alert Alert You have showered for ".split(" ").join("%20") 
+                + request.params.showerTime 
+                + "&return_url=1";
+    
+    Parse.Cloud.httpRequest({
+        url: ttsURL,
+        success: function(httpResponse) {
+            // Push to HKRules phone
+            Parse.Push.send({
+                where: pushQuery,
+                data: {
+                    "alert": "You showered for " + request.params.showerTime.replace("+", " ") + "!",
+                    "content-available": 1,
+                    "ttsURL":  httpResponse.text
                 },
-                    error: function(error) {
-                    response.error("push errored");         
-                }
-                }); //end push
+                    push_time: alertTime
+            },{ success: function() {
+                response.success("push for " + request.params.username + " scheduled.");
             },
-            error: function(httpResponse) {
-            response.error(httpResponse)
+                error: function(error) {
+                response.error("push errored");         
             }
-        });
-                   
-    },
-        error: function(error) {
-		    response.error("user find query errored");
+            }); //end push
+        },
+        error: function(httpResponse) {
+            response.error(httpResponse)
         }
-                   
-    });
+    });        
 });
 
 // Called when client is about to leave the house 
