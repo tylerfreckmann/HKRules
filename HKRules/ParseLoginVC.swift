ca62e831a513aa400b808910877a3309694b6e1a
Eric Tan <Eric@SparkPR-Image.local>
2015-08-17 15:14:13 -0700

    touched up login and sign up vc with harman logo

--- HKRules/ParseLoginVC.swift
+++ HKRules/ParseLoginVC.swift
@@ -6,63 +6,40 @@
 //  Copyright (c) 2015 Tyler Freckmann. All rights reserved.
 //
 
+
 import Foundation
 import UIKit
 import Parse
 import ParseUI
 
-class ParseLoginVC: PFLogInViewController, PFLogInViewControllerDelegate, PFSignUpViewControllerDelegate{
-    
-    let logInViewController = PFLogInViewController()
-    let signInViewController = PFSignUpViewController()
+class ParseLoginVC: PFLogInViewController, PFSignUpViewControllerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.darkGrayColor()
+        
+        var traits = (PFLogInFields.UsernameAndPassword
+            | PFLogInFields.LogInButton
+            | PFLogInFields.SignUpButton
+            | PFLogInFields.PasswordForgotten
+            | PFLogInFields.Facebook)
+        
+        // Set log in traits
+        self.fields = traits
+        
+        // Set the logo to Harman in loginVC
+        let harmanLoginLogo = UIImageView(image: UIImage(named:"Harman_Logo.png"))
+        self.logInView!.logo = harmanLoginLogo
+        
     }
     
     override func viewDidAppear(animated: Bool) {
         if (PFUser.currentUser() != nil) {
             self.performSegueWithIdentifier("goToScenarios", sender: self)
         }
-        else {
-            // Set log in traits
-            logInViewController.fields = (PFLogInFields.UsernameAndPassword
-                | PFLogInFields.LogInButton
-                | PFLogInFields.SignUpButton
-                | PFLogInFields.PasswordForgotten)
-            logInViewController.delegate = self
-            self.presentViewController(logInViewController, animated: false, completion: nil)
-            
-            // Set sign up traits
-            signInViewController.fields = (PFSignUpFields.UsernameAndPassword
-                | PFSignUpFields.SignUpButton
-                | PFSignUpFields.Email
-                | PFSignUpFields.Additional
-                | PFSignUpFields.DismissButton)
-            signInViewController.signUpView?.additionalField?.placeholder = "Name"
-            signInViewController.delegate = self
-            logInViewController.signUpController = signInViewController
-        }
     }
     
-    func logInViewController(controller: PFLogInViewController, didLogInUser user: PFUser) -> Void {
-        updateInstallation()
-        self.dismissViewControllerAnimated(true, completion: nil)
-    }
-    
-    func signUpViewController(signUpController: PFSignUpViewController, didSignUpUser user: PFUser) -> Void {
-        updateInstallation()
-        self.dismissViewControllerAnimated(true, completion: nil)
-    }
-    
-    func updateInstallation() {
-        let installation = PFInstallation.currentInstallation();
-        installation["user"] = PFUser.currentUser();
-        installation.saveInBackgroundWithBlock { (success, error) -> Void in
-            if error != nil {
-                println(error)
-            }
-        }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.logInView!.logo?.frame = CGRect(x: 30, y: 30, width: 350, height: 270)
     }
 }
